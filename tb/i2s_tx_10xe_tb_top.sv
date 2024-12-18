c8231c19f8b21b3c7c4f4cedb2fb24c2bd41abdd
engnr-junaid <mjunaidahmed469@gmail.com>
2024-12-18 12:48:34 +0500

    tb top file added which will get mature by time

---
+++ tb/i2s_tx_10xe_tb_top.sv
@@ -0,0 +1,14 @@
+/*************************************************************************
+   > File Name: i2s_tx_10xe_tb_top.sv
+   > Description: This file defines the AXI-Stream interface signals and associated clocking blocks for I2S transmitter functionality.
+   > Author: Muhammad Junaid Ramzan
+   > Modified: Muhammad Junaid Ramzan
+   > Mail: muhammad.junaid@10xengineers.ai
+   ---------------------------------------------------------------
+   Copyright   (c)2024 10xEngineers
+   ---------------------------------------------------------------
+************************************************************************/
+
+`include "i2s_tx_10xe_axi_stream_intf.sv"
+`include "i2s_tx_10xe_axi4_lite_intf.sv"
+`include "i2s_tx_10xe_dut_intf.sv"
