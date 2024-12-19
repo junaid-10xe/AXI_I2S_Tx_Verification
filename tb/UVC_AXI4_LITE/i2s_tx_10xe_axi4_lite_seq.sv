d2532602f138a256a11d88d3303bb41d891fb1b6
engnr-junaid <mjunaidahmed469@gmail.com>
2024-12-19 16:16:05 +0500

    Added sequence for reading registers and ran that simulation everything worked fine

--- tb/UVC_AXI4_LITE/i2s_tx_10xe_axi4_lite_seq.sv
+++ tb/UVC_AXI4_LITE/i2s_tx_10xe_axi4_lite_seq.sv
@@ -24,33 +24,33 @@ class i2s_tx_10xe_axi4_lite_seq extends uvm_sequence;
 
     
     
-    task pre_body();
-        uvm_phase phase;
-        `ifdef UVM_VERSION_1_2
-          // in UVM1.2, get starting phase from method
-          phase = get_starting_phase();
-        `else
-          phase = starting_phase;
-        `endif
-        if (phase != null) begin
-          phase.raise_objection(this, get_type_name());
-          `uvm_info(get_type_name(), "raise objection", UVM_MEDIUM)
-        end
-      endtask : pre_body
+    // task pre_body();
+    //     uvm_phase phase;
+    //     `ifdef UVM_VERSION_1_2
+    //       // in UVM1.2, get starting phase from method
+    //       phase = get_starting_phase();
+    //     `else
+    //       phase = starting_phase;
+    //     `endif
+    //     if (phase != null) begin
+    //       phase.raise_objection(this, get_type_name());
+    //       `uvm_info(get_type_name(), "raise objection", UVM_MEDIUM)
+    //     end
+    //   endtask : pre_body
     
-    task post_body();
-        uvm_phase phase;
-        `ifdef UVM_VERSION_1_2
-          // in UVM1.2, get starting phase from method
-          phase = get_starting_phase();
-        `else
-          phase = starting_phase;
-        `endif
-        if (phase != null) begin
-          phase.drop_objection(this, get_type_name());
-          `uvm_info(get_type_name(), "drop objection", UVM_MEDIUM)
-        end
-      endtask : post_body
+    // task post_body();
+    //     uvm_phase phase;
+    //     `ifdef UVM_VERSION_1_2
+    //       // in UVM1.2, get starting phase from method
+    //       phase = get_starting_phase();
+    //     `else
+    //       phase = starting_phase;
+    //     `endif
+    //     if (phase != null) begin
+    //       phase.drop_objection(this, get_type_name());
+    //       `uvm_info(get_type_name(), "drop objection", UVM_MEDIUM)
+    //     end
+    //   endtask : post_body
     
     
     
