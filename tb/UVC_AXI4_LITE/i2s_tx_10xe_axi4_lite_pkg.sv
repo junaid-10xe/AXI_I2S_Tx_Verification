9dc7b4da740c0865b14c31f5a7590229ac3422da
engnr-junaid <mjunaidahmed469@gmail.com>
2024-12-21 14:35:15 +0500

    ADDED pkg file for axi4_lite used to include all necessary files

--- tb/UVC_AXI4_LITE/i2s_tx_10xe_axi4_lite_pkg.sv
+++ tb/UVC_AXI4_LITE/i2s_tx_10xe_axi4_lite_pkg.sv
@@ -1,5 +1,5 @@
 /*************************************************************************
-   > File Name: i2s_tx_10xe_axis_pkg.sv
+   > File Name: i2s_tx_10xe_axi4_lite_pkg.sv
    > Description: This file defines the AXI-Stream Package to include necsaary files required for UVC of AXI-Stream.
    > Author: Muhammad Junaid Ramzan
    > Modified: Muhammad Junaid Ramzan
@@ -8,9 +8,9 @@
    Copyright   (c)2024 10xEngineers
    ---------------------------------------------------------------
 ************************************************************************/
-`ifndef I2S_TX_10XE_AXIS_PKG
-`define I2S_TX_10XE_AXIS_PKG
-package i2s_tx_10xe_axis_pkg;
+`ifndef I2S_TX_10XE_AXI4_LITE_PKG
+`define I2S_TX_10XE_AXI4_LITE_PKG
+package i2s_tx_10xe_axi4_lite_pkg;
     // import the UVM library
   	import uvm_pkg::*;
 
@@ -21,13 +21,13 @@ package i2s_tx_10xe_axis_pkg;
     //TODO
 
     //include UVC files
-    `include "i2s_tx_10xe_axi_stream_intf.sv"
-    `include "i2s_tx_10xe_axis_seq_item.sv"
-    `include "i2s_tx_10xe_axis_seq.sv"
-    `include "i2s_tx_10xe_axis_sequencer.sv"
-    `include "i2s_tx_10xe_axis_monitor.sv"
-    `include "i2s_tx_10xe_axis_driver.sv"
-    `include "i2s_tx_10xe_axis_agent.sv"
+    `include "i2s_tx_10xe_axi4_lite_intf.sv"
+    `include "i2s_tx_10xe_axi4_lite_seq_item.sv"
+    `include "i2s_tx_10xe_axi4_lite_seq.sv"
+    `include "i2s_tx_10xe_axi4_lite_sequencer.sv"
+    `include "i2s_tx_10xe_axi4_lite_monitor.sv"
+    `include "i2s_tx_10xe_axi4_lite_driver.sv"
+    `include "i2s_tx_10xe_axi4_lite_agent.sv"
 endpackage
 
 `endif
