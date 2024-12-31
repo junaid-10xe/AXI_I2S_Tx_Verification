5ac303c7ac2bd5e9bbcad675141aa5fb963f956a
engnr-junaid <mjunaidahmed469@gmail.com>
2024-12-31 12:39:47 +0500

    ADDED file i2s_tx_10xe_reg_seqs.sv

---
+++ tb/UVM_RAL/i2s_tx_10xe_reg_seqs.sv
@@ -0,0 +1,19 @@
+/*************************************************************************
+   > File Name: i2s_tx_10xe_reg_seqs.sv
+   > Description: This file defines the Sequences to test RAL.
+   > Author: Muhammad Junaid Ramzan
+   > Modified: Muhammad Junaid Ramzan
+   > Mail: muhammad.junaid@10xengineers.ai
+   ---------------------------------------------------------------
+   Copyright   (c)2024 10xEngineers
+   ---------------------------------------------------------------
+************************************************************************/
+`ifndef I2S_TX_10XE_REG_SEQS
+`define I2S_TX_10XE_REG_SEQS
+//Class :: i2s_tx_reg_base_seq
+
+class i2s_tx_reg_base_seq extends uvm_sequence;
+    
+endclass
+
+`endif
