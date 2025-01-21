3d0777ef37e810e2f73a9d99fc4f23d44be4c70c
engnr-junaid <mjunaidahmed469@gmail.com>
2025-01-21 18:29:36 +0500

    Updated tb top

--- tb/top/i2s_tx_tb_top.sv
+++ tb/top/i2s_tx_tb_top.sv
@@ -11,33 +11,8 @@
 `ifndef I2S_TX_TB_TOP
 `define I2S_TX_TB_TOP
     `timescale 1ns/1ns
-    // include the UVM macros
-    `include "uvm_macros.svh"
-    // import the UVM library
-  	import uvm_pkg::*;
-
-    // Import defines
-    import i2s_tx_defines::*;
-    // Include Interfaces
-    `include "../UVC_AXI4_LITE/i2s_tx_axi4_lite_intf.sv"
-    `include "../UVC_AXI_STREAM/i2s_tx_axi_stream_intf.sv"
-    `include "../I2S_RSP_AGENT/i2s_tx_intf.sv"
-
-    
-    // Import AXI4-LITE UVC PKG to include files
-    import i2s_tx_axi4_lite_pkg::*;
-    
-    // Import AXI-STream PKG to include files
-    import i2s_tx_axis_pkg::*;
-    // Import DUT PKG to include files
-    import i2s_tx_rsp_pkg::*;
-    
-    // Include files
-    `include "../scoreboard/i2s_tx_scoreboard.sv"
-    `include "../coverage/i2s_tx_coverage.sv"
-    `include "../env/i2s_tx_env.sv"
-    `include "../test_top/i2s_tx_base_test.sv"
-    `include "../test_top/i2s_tx_tests_lib.sv"
+    // Include file which add all necessary files
+    `include "../include/i2s_tx_include_files.sv"
 
  
 module i2s_tx_tb_top;
